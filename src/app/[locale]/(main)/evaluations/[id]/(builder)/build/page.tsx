7d22ca901a427192746d6bb17f8e943462e27693
angel estrada <rangel1998.rt@gmail.com>
2025-07-16 05:42:36 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/[locale]/(main)/evaluations/[id]/(builder)/build/page.tsx
+++ src/app/[locale]/(main)/evaluations/[id]/(builder)/build/page.tsx
@@ -2,10 +2,10 @@
 
 import { FormBuilderContent } from '@/components/evaluations/builder/form-builder-content';
 
-export default function FormBuilderPage({ params }: { params: { id: string } }) {
+export default function FormBuilderPage({ params: { id } }: { params: { id: string } }) {
     // Here, we are in a Server Component. We can access params directly.
     // We then pass the 'id' as a simple string prop to our Client Component.
     return (
-        <FormBuilderContent evaluationId={params.id} />
+        <FormBuilderContent evaluationId={id} />
     );
 }
