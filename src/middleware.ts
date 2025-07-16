de2b874cc9e56428c223e513a8047d92c83a9456
angel estrada <rangel1998.rt@gmail.com>
2025-07-16 05:19:52 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,11 @@ import createMiddleware from 'next-intl/middleware';
 import {locales, localePrefix} from '@/navigation';
  
 export default createMiddleware({
+  // A list of all locales that are supported
   locales,
   localePrefix,
+ 
+  // Used when no locale matches
   defaultLocale: 'es',
 });
  
@@ -11,5 +14,5 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/', '/((?!api|_next|_vercel|.*\\..*).*)']
 };
